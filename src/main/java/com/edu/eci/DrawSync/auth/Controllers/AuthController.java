94b9fef58a2ad666fc724c2956af6ed157620a29
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-11-24 13:50:53 -0500

    Fix package naming inconsistency in SecurityConfig and WebSocketConfig

--- src/main/java/com/edu/eci/DrawSync/auth/Controllers/AuthController.java
+++ src/main/java/com/edu/eci/DrawSync/auth/Controllers/AuthController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -92,7 +93,6 @@ public class AuthController {
             "message", "Password recovery initiated successfully"
         ));
     }
-    
     
     @PostMapping("/confirm_password")
     public ResponseEntity<?> forgotPassword(@RequestBody ReocoverPassword request) {
